0d66cd9cb5c01dec9dcfa2ad5a5096c740e632bd
Bingreee <shbyun9808@gmail.com>
2022-04-04 09:52:45 +0900

    campy commit test

---
+++ src/main/java/campy/com/CampyApplication.java
@@ -0,0 +1,13 @@
+package campy.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/campy/com/CampyApplicationTests.java
@@ -0,0 +1,13 @@
+package campy.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
