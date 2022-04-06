e402f74781d207a0374ea5bb469c528d94648722
Bingreee <shbyun9808@gmail.com>
2022-04-06 16:06:23 +0900

    예약 관련 클래스 수정 day0406

--- src/main/java/campy/com/service/ReserveService.java
+++ src/main/java/campy/com/service/ReserveService.java
@@ -4,10 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import campy.com.dao.ReserveDao;
+import campy.com.dto.ReserveDto;
 
 @Service
 public class ReserveService {
 
 	@Autowired
 	ReserveDao rdao;
+	
+	public ReserveDto reserveStatus() {
+		return rdao.reserveStatus();
+	}
+	
+	
+	public int deleteReserve(ReserveDto dto) {
+		return rdao.deleteReserve(dto);
+	}
 }
