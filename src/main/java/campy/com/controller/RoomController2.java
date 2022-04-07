5a6bb4df198d0e5aa6f9fb1f9103c0a4f30a3372
Bingreee <shbyun9808@gmail.com>
2022-04-07 14:34:47 +0900

    예약 현황 페이지 수정/객실 검색 클래스들 수정

--- src/main/java/campy/com/controller/RoomController2.java
+++ src/main/java/campy/com/controller/RoomController2.java
@@ -1,8 +1,19 @@
 package campy.com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import campy.com.service.RoomService2;
 
 @Controller
 public class RoomController2 {
 
+	@Autowired
+	RoomService2 rservice2;
+	
+	@GetMapping("/room")
+	public String room() {
+		return "room";
+	}
 }
