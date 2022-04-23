e0f5a1832234b1bfe5ab32313aca7a84ef507efd
Bingreee <shbyun9808@gmail.com>
2022-04-23 17:50:25 +0900

    0423

---
+++ src/main/java/campy/com/controller/SecurityController.java
@@ -0,0 +1,38 @@
+package campy.com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+
+//	@GetMapping("/")
+//	public String showmain() {
+//		System.out.println("메인으로");
+//		return "main";
+//	}
+//	
+//	@GetMapping("/member")
+//	public String forMember() {
+//		System.out.println("member요청");
+//		return "main";
+//	}
+//	
+//	@GetMapping("/admin")
+//	public String forAdmin() {
+//		System.out.println("admin 요청");
+//		return "adminPage";
+//	}
+//	
+//	@GetMapping("/campyLogin")
+//	public String campyLogin() {
+//		System.out.println("campyLogin 요청");
+//		return "login";
+//	}
+//	
+//	@GetMapping("/loginSuccess")
+//	public String loginSuccess() {
+//		System.out.println("로그인 성공");
+//		return "main";
+//	}
+}
